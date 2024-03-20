a097b9eda54739bdf3a60c6b5c26f3a19d34acbf
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2024-03-21 04:21:53 +0600

    add sticky image

--- src/components/pages/Home/ExtText.jsx
+++ src/components/pages/Home/ExtText.jsx
@@ -4,7 +4,7 @@ import { motion } from "framer-motion";
 const ExtText = () => {
     return (
         <div className="w-full">
-            <motion.h1 className="md:min-h-60 text-2xl md:text-5xl lg:text-6xl tracking-widest px-5 md:px-20"
+            <motion.h1 style={{fontSize:"60px", lineHeight:"84px"}} className="md:min-h-60 text-2xl md:text-5xl lg:text-6xl tracking-widest px-5 md:px-20"
                 initial={{ opacity: 0 }}
                 animate={{ opacity: 1 }}
                 transition={{ duration: 1 }}
@@ -15,7 +15,7 @@ const ExtText = () => {
             <div className="w-full flex flex-col md:flex-row justify-between items-center gap-5 px-5 md:px-20 py-20">
                 <h2 data-aos="fade-up" data-aos-duration="500" className="w-full md:w-2/5 text-2xl md:text-5xl">X - <br className="hidden md:block" />
                     The Moonshot Factory</h2>
-                <h5 data-aos="fade-up" data-aos-duration="500" className="w-full md:w-3/5 text-md md:text-2xl md:pr-16" style={{ lineHeight: "1.8" }}>X is a diverse group of inventors and entrepreneurs who build and launch technologies that aim to improve the lives of millions, even billions, of people. Our goal: 10x impact on the world’s most intractable problems, not just 10% improvement. We approach projects that have the aspiration and riskiness of research with the speed and ambition of a startup.</h5>
+                <h5 data-aos="fade-up" data-aos-duration="1500" className="w-full md:w-3/5 text-md md:text-2xl md:pr-16" style={{ lineHeight: "1.8" }}>X is a diverse group of inventors and entrepreneurs who build and launch technologies that aim to improve the lives of millions, even billions, of people. Our goal: 10x impact on the world’s most intractable problems, not just 10% improvement. We approach projects that have the aspiration and riskiness of research with the speed and ambition of a startup.</h5>
             </div>
         </div >
     );
--- src/components/pages/Home/PublicXProject.jsx
+++ src/components/pages/Home/PublicXProject.jsx
@@ -1,23 +1,78 @@
+import { useEffect, useState } from "react";
 import { FaArrowRight } from "react-icons/fa";
 
 const PublicXProject = () => {
+    const [isSticky, setIsSticky] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const offset = window.scrollY;
+            if (offset > 1500) {
+                setIsSticky(true);
+            } else {
+                setIsSticky(false);
+            }
+        };
+
+        window.addEventListener("scroll", handleScroll);
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
     return (
-        <div className='w-full'>
-            <div className="bg-[#FFB300] w-80 h-96 px-20 py-7 sticky">
-                <h3 className="uppercase font-bold">Public X Projects</h3>
+        <div className='w-full relative pt-40'>
+            <div className={`bg-[#FFB300] w-96 h-52 px-20 py-7 ${isSticky ? 'sticky top-0 z-20 bg-transparent' : 'sticky top-5'}`}>
+                <h3 className={`uppercase text-xl font-semibold ${isSticky ? 'text-white z-20' : 'text-black z-20'}`}> <div className={`h-2 w-20 rounded-full bg-white absolute top-10 -left-14 ${isSticky ? 'block' : 'hidden'}`}></div> Public X Projects</h3>
             </div>
-            <div className="bg-[url('/img_1.webp')] bg-cover bg-center w-full h-screen">
-                <div className="w-full h-full flex flex-col justify-start items-center pl-10">
-                    <div className="w-full h-full flex items-center mt-20">
-                        <h3 className="text-white text-2xl md:text-3xl">What if beams of light could provide <br />
-                            global access to fast, affordable connectivity?</h3>
+            <div className="relative -mt-20 z-0">
+                <div data-aos="fade-up" className="bg-[url('/img_1.webp')] bg-fixed h-screen w-full bg-cover bg-center">
+
+                    <div className="text-white p-10 h-full w-full flex items-center">
+                        <div className="w-full space-y-10">
+                            <div className="w-full">
+                                <h3 style={{ fontFamily: "Roboto" }} className="text-white text-2xl md:text-3xl">What if beams of light could provide <br />
+                                    global access to fast, affordable connectivity?</h3>
+                            </div>
+                            <div className="text-2xl text-black group cursor-pointer h-full md:text-3xl flex justify-start text-start w-full gap-7 ">
+                                <div className="text-3xl bg-[#F5F5F5] group-hover:bg-[#FFB300] rounded w-12 h-12 flex justify-center items-center overflow-x-hidden"><FaArrowRight className="-translate-x-10 group-hover:translate-x-3 duration-200 " /> <FaArrowRight className=" -translate-x-3 group-hover:translate-x-12 duration-200" /></div>
+                                <h3 className="text-white group-hover:text-[#FFB300] pt-2">Explore Taara</h3>
+                            </div>
+                        </div>
                     </div>
-                    <div className="text-2xl group cursor-pointer h-full md:text-3xl flex justify-start text-start w-full gap-7 -mt-10">
-                        <div className="text-3xl bg-[#F5F5F5] group-hover:bg-[#FFB300] rounded w-12 h-12 flex justify-center items-center overflow-x-hidden"><FaArrowRight className="-translate-x-10 group-hover:translate-x-3 duration-200 " /> <FaArrowRight className=" -translate-x-3 group-hover:translate-x-12 duration-200" /></div>
-                        <h3 className="text-white group-hover:text-[#FFB300] pt-2">Explore Taara</h3>
+
+                </div>
+                <div data-aos="fade-up" className="bg-[url('/img_2.webp')] bg-fixed h-screen w-full bg-cover bg-center">
+
+                    <div className="text-white p-10 h-full w-full flex items-center">
+                        <div className="w-full space-y-10">
+                            <div className="w-full">
+                                <h3 className="text-white text-2xl md:text-3xl">What if we could make the world’s electric grid <br /> visible</h3>
+                                <h3 className="text-white text-2xl md:text-3xl">so everyone could access clean, reliable <br /> energy</h3>
+                            </div>
+                            <div className="text-2xl text-black group cursor-pointer h-full md:text-3xl flex justify-start text-start w-full gap-7 ">
+                                <div className="text-3xl bg-[#F5F5F5] group-hover:bg-[#FFB300] rounded w-12 h-12 flex justify-center items-center overflow-x-hidden"><FaArrowRight className="-translate-x-10 group-hover:translate-x-3 duration-200 " /> <FaArrowRight className=" -translate-x-3 group-hover:translate-x-12 duration-200" /></div>
+                                <h3 className="text-white group-hover:text-[#FFB300] pt-2">Explore Tapestry</h3>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <div data-aos="fade-up" className="bg-[url('/img_3.webp')] bg-fixed h-screen w-full bg-cover bg-center">
+
+                    <div className="text-white p-10 h-full w-full flex items-center">
+                        <div className="w-full space-y-10">
+                            <div className="w-full">
+                                <h3 className="text-white text-2xl md:text-3xl">What if we could protect the ocean <br />
+                                    while feeding the world sustainably?</h3>
+                            </div>
+                            <div className="text-2xl text-black group cursor-pointer h-full md:text-3xl flex justify-start text-start w-full gap-7 ">
+                                <div className="text-3xl bg-[#F5F5F5] group-hover:bg-[#FFB300] rounded w-12 h-12 flex justify-center items-center overflow-x-hidden"><FaArrowRight className="-translate-x-10 group-hover:translate-x-3 duration-200 " /> <FaArrowRight className=" -translate-x-3 group-hover:translate-x-12 duration-200" /></div>
+                                <h3 className="text-white group-hover:text-[#FFB300] pt-2">Explore Tidal</h3>
+                            </div>
+                        </div>
                     </div>
                 </div>
-
             </div>
         </div>
     );

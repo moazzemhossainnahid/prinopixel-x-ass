09c5f8c3c6edb7c38ba964344b927f1173c19291
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2024-03-21 14:08:37 +0600

    resolved bg

--- src/components/pages/Home/XTimeline.jsx
+++ src/components/pages/Home/XTimeline.jsx
@@ -28,25 +28,25 @@ const XTimeline = () => {
                 <h3 className={`uppercase tracking-widest text-xl font-semibold ${isSticky ? 'text-white z-20' : 'text-black z-20'}`}> <div className={`h-2 w-20 rounded-full bg-white absolute top-10 -left-14 ${isSticky ? 'block' : 'hidden'}`}></div> X Timeline</h3>
             </div>
             <div className="relative -mt-20 z-0">
-                <div data-aos="fade-up" className="bg-black bg-inherit h-screen w-full bg-cover bg-center">
+                <div data-aos="fade-up" className="bg-black h-screen w-full">
 
-                    <div className="text-white p-10 h-full w-full flex items-center">
+                    <div className="text-white bg-black p-10 h-full w-full flex items-center">
                         <div className="w-full space-y-10">
-                        <motion.h3
-                initial={{ y: 100, opacity: 0 }}
-                animate={{ y: 0, opacity: 1, transition: { duration: 1, delay: 0.5 }}}
-                style={{ fontFamily: "Roboto" }}
-                className="text-white text-2xl md:text-3xl"
-            >
-                Ten years of moonshots
-            </motion.h3>
-            <motion.p
-                initial={{ y: 100, opacity: 0 }}
-                animate={{ y: 0, opacity: 1, transition: { duration: 1, delay: 1 } }}
-                className="text-md md:text-xl tracking-wider pt-10"
-            >
-                In 2010, Google founders Larry Page and Sergey Brin decided to form a new division of the company to work on moonshots: far-out, sci-fi sounding technologies that could one day make the world a radically better place. It was a grand experiment — some might say a moonshot unto itself. 10 years in, X has incubated hundreds of different moonshot projects, many of which have gone on to become independent businesses.
-            </motion.p>
+                            <motion.h3
+                                initial={{ y: 100, opacity: 0 }}
+                                animate={{ y: 0, opacity: 1, transition: { duration: 1, delay: 0.5 } }}
+                                style={{ fontFamily: "Roboto" }}
+                                className="text-white text-2xl md:text-3xl"
+                            >
+                                Ten years of moonshots
+                            </motion.h3>
+                            <motion.p
+                                initial={{ y: 100, opacity: 0 }}
+                                animate={{ y: 0, opacity: 1, transition: { duration: 1, delay: 1 } }}
+                                className="text-md md:text-xl tracking-wider pt-10"
+                            >
+                                In 2010, Google founders Larry Page and Sergey Brin decided to form a new division of the company to work on moonshots: far-out, sci-fi sounding technologies that could one day make the world a radically better place. It was a grand experiment — some might say a moonshot unto itself. 10 years in, X has incubated hundreds of different moonshot projects, many of which have gone on to become independent businesses.
+                            </motion.p>
                         </div>
                     </div>
 

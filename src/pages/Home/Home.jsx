664e36a287c92037dd80b9603299d5b52e365bf7
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2024-03-21 13:04:08 +0600

    resolved news

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import PublicXProject from "../../components/pages/Home/PublicXProject";
 import ExtText from "../../components/pages/Home/ExtText";
+import StoriesNews from "../../components/pages/Home/StoriesNews";
 
 const Home = () => {
     return (
         <div className="w-full">
             <ExtText/>
             <PublicXProject/>
+            <StoriesNews/>
         </div>
     );
 };
